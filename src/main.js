cae87f7bbbb99e0dc582e31fb979e3ea18fd97f7
chen <53075695@qq.com>
2022-09-29 09:49:16 +0800

    实战项目

--- src/main.js
+++ src/main.js
@@ -5,6 +5,13 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+// 引入element
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
+
+
 new Vue({
   router,
   store,
